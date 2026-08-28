0656bb498402c5b97524abddfd02de9d032b360c
igorblacconaro <igorbs2910@gmail.com>
2026-08-28 19:53:20 -0300

    feat: Página NotFound criada e escopo inicial com  link para retornar a "Home" adicionados.

---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+
+export default function NotFound(){
+    return(
+
+        <main>
+            
+            <h2>Erro - 404</h2>
+            <h3>Página não encontrada</h3>
+
+            <p>O endereço que você tentou acessar não existe.</p>
+
+            <Link to="/">
+            Voltar para página inicial.
+            </Link>
+
+
+        </main>
+
+    );
+}
