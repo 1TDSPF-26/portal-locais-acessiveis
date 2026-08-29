e0bfd672bc4213e321ad4684e78909731e213b85
GustavoJardimSa <gujardimdesa@gmail.com>
2026-08-29 14:18:48 -0300

    feat: link do header feito, junto da  troca de abas pelo o botão TAB, feito o css basico para a identificação de botões

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,35 @@
+import {NavLink} from "react-router-dom";
+import "./Header.css";
+
 export function Header() {
   return (
     <header>
       <h1>Portal de Locais e Serviços Acessíveis</h1>
+      <nav className="mainNav" aria-label="Menu Principal">
+        <ul>
+          
+          <li>
+            <NavLink to="/" end>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/locais">
+              Locais
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/cadastrar">
+              Cadastro"
+            </NavLink>"
+          </li>
+          <li>
+            <NavLink to="/sobre">
+              Sobre
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
