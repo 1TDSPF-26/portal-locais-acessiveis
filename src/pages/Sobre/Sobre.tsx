976defe6322f9db0c3eba5bee2e2adba6ecef85b
giovvane <giovanedc007@gmail.com>
2026-09-06 12:33:47 -0300

    feat: ajustes de linguagem na página sobre

--- src/pages/Sobre/Sobre.tsx
+++ src/pages/Sobre/Sobre.tsx
@@ -1,10 +1,27 @@
 function Sobre() {
   return (
     <div>
-      <h1>Sobre</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto et est explicabo beatae neque laborum quasi non nemo ab, illo tempore ducimus ipsam perferendis nobis, ratione minima voluptas sint expedita!</p>
+      <header>
+        <h1>Sobre</h1>
+        <p>
+          Nesta página será explicado mais a fundo o porque deste projeto existir, suas intenções e o porque de ser tão importante
+        </p>
+      </header>
+      <section>
+        <h2>
+          Porque o projeto existe e sua importância social
+        </h2>
+        <p>Este projeto foi idealizado com a intenção de conscientização para um público geral, para aqueles que já tem conhecimento do tema
+          e para aqueles que ainda não.<br />
+          Aqui, gostaríamos de apontar que todas as pessoas merecem poder fazer o que querem, porém isso às vezes é interrompido por alguma barreira.
+          Nem todo local (seja ele um lugar físico ou um site por exemplo) atende as necessidades de acessibilidade de todas as pessoas, como exemplos
+          simplórios, uma rampa de acesso para cadeirantes, ou um adaptador de cores de um site para um daltônico, esse tipo de coisa é o mínimo 
+          aceitável de acessibilidade que uma pessoa merece.<br />
+          Pessoas com deficiência de modo geral, não devem se sentir diferentes, elas devem se sentir como qualquer outra, recebendo os devidos
+          benefícios para que possam viver a vida, o que é um direito do ser humano, ninguém merece menos do que ninguém por ser diferente.<br />
+        </p>
+      </section>
     </div>
   )
 }
-
 export default Sobre
