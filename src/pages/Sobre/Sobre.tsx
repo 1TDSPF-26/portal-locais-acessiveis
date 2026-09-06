f618b633ebd350f3879e67ffe48a8b100194ff52
giovvane <giovanedc007@gmail.com>
2026-09-06 12:46:36 -0300

    feat: ajuste de linguagem na página sobre

--- src/pages/Sobre/Sobre.tsx
+++ src/pages/Sobre/Sobre.tsx
@@ -4,19 +4,14 @@ function Sobre() {
       <header>
         <h1>Sobre</h1>
         <p>
-<<<<<<< HEAD
           Nesta página será explicado mais a fundo o porque deste projeto existir, suas intenções e o porque de ser tão importante
-=======
-          Nesta página será explicado mais afundo o porque deste projeto existir, suas intenções e o porque de ser tão importante
->>>>>>> ec2218fbd93de60cd434bb90de82f9d50632855b
         </p>
       </header>
       <section>
         <h2>
           Porque o projeto existe e sua importância social
         </h2>
-<<<<<<< HEAD
-        <p>Este projeto foi idealizado com a intenção de conscientização para um público geral, para aqueles que já tem conhecimento do tema
+         <p>Este projeto foi idealizado com a intenção de conscientização para um público geral, para aqueles que já tem conhecimento do tema
           e para aqueles que ainda não.<br />
           Aqui, gostaríamos de apontar que todas as pessoas merecem poder fazer o que querem, porém isso às vezes é interrompido por alguma barreira.
           Nem todo local (seja ele um lugar físico ou um site por exemplo) atende as necessidades de acessibilidade de todas as pessoas, como exemplos
@@ -24,23 +19,10 @@ function Sobre() {
           aceitável de acessibilidade que uma pessoa merece.<br />
           Pessoas com deficiência de modo geral, não devem se sentir diferentes, elas devem se sentir como qualquer outra, recebendo os devidos
           benefícios para que possam viver a vida, o que é um direito do ser humano, ninguém merece menos do que ninguém por ser diferente.<br />
-=======
-        <p>Este projeto foi idealizado com a inteção de conscientização para um público geral, para aqueles que já tem conhecimento do tema
-          e para aqueles que ainda não.<br />
-          Aqui, gostaríamos de apontar que todas as pessoas merecem poder fazer o que querem, porém isso às vezes é interrompido por alguma barreira.
-          Nem todo local (seja ele um lugar físico ou um site por exemplo) atende as dependências de pessoas com certas dificuldades, como exemplos
-          simplórios, uma rampa de acesso para cadeirantes, ou um adaptador de cores de um site para um daltônico, esse tipo de coisa é o mínimo 
-          aceitável de acessibilidade que uma pessoa merece.<br />
-          Pessoas deficientes de modo geral, não devem se sentir menos favorecidas, elas devem se sentir como qualquer outra, recendo os devidos
-          benefícios para que possam viver a vida, o que é um direito do ser humano, nimguém merece menos do que ninguém por ser diferente.<br />
->>>>>>> ec2218fbd93de60cd434bb90de82f9d50632855b
         </p>
       </section>
     </div>
   )
 }
-<<<<<<< HEAD
+
 export default Sobre
-=======
-export default Sobre
->>>>>>> ec2218fbd93de60cd434bb90de82f9d50632855b
