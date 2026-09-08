f93981c5f230c00c83b3e7cedc01dfb7b01ec7cc
giovvane <giovanedc007@gmail.com>
2026-09-08 20:28:30 -0300

    fix: remove marcações de conflito não resolvidas

--- src/pages/Sobre/Sobre.tsx
+++ src/pages/Sobre/Sobre.tsx
@@ -1,8 +1,25 @@
 function Sobre() {
   return (
     <div>
-      <h1>Sobre</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto et est explicabo beatae neque laborum quasi non nemo ab, illo tempore ducimus ipsam perferendis nobis, ratione minima voluptas sint expedita!</p>
+      <header>
+        <h1>Sobre</h1>
+        <p>
+          Nesta página será explicado mais a fundo o porquê deste projeto existir, suas intenções e o porquê de ser tão importante
+        </p>
+      </header>
+      <section>
+        <h2>
+          Por que o projeto existe e sua importância social
+        </h2>
+         <p>Este projeto foi idealizado com a intenção de conscientização para um público geral, para aqueles que já têm conhecimento do tema
+          e para aqueles que ainda não.
+            Aqui, gostaríamos de apontar que todas as pessoas merecem poder fazer o que querem, porém isso às vezes é impedido por alguma barreira.
+            Nem todo local (seja ele um lugar físico ou um site) atende as necessidades de acessibilidade de todas as pessoas. Alguns exemplos são uma 
+            rampa de acesso para cadeirantes ou cores adaptadas para facilitar a leitura de pessoas com daltonismo, esse tipo de coisa é o mínimo 
+          aceitável de acessibilidade que uma pessoa merece.
+            Pessoas com deficiência merecem as mesmas oportunidades que qualquer outra pessoa, recebendo a devida inclusão social.
+        </p>
+      </section> 
     </div>
   )
 }
