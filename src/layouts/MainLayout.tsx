7a974933b7025967ea23a41e100bb42ff813ecd5
rodrigo15511 <rodrigotcosta2006@gmail.com>
2026-09-10 15:32:02 -0300

    feat:Adicionando link parar ir pro contudo principal

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -9,9 +9,16 @@ interface MainLayoutProps {
 export function MainLayout({ children }: MainLayoutProps) {
   return (
     <>
+      <a
+        href="#conteudo-principal"
+        className="sr-only focus:not-sr-only focus:absolute focus:z-50 focus:p-2 focus:bg-white focus:text-black focus:outline focus:outline-2"
+      >
+        Pular para o conteúdo principal
+      </a>
+
       <Header />
 
-      <main>
+      <main id="conteudo-principal">
         {children}
       </main>
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter> {/*ele faz com que o react-router funcione  para navegar entre as paginas do site*/}
