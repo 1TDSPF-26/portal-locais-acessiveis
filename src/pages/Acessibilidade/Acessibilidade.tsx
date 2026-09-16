7e265805b03c809263ac682044dbfe3739ebc6a2
lenahlawi07 <lynnhalawi07@gmail.com>
2026-09-16 19:24:12 -0300

    feat : criar pagina Acessibilidade com estrutura e h1.

---
+++ src/pages/Acessibilidade/Acessibilidade.tsx
@@ -0,0 +1,13 @@
+function Acessibilidade() {
+
+    return(
+        <div>
+            <header>
+                <h1>Acessibilidade</h1>
+                <p>Conteúdo em construção.</p>
+            </header>
+        </div>
+    )
+}
+
+export default Acessibilidade
