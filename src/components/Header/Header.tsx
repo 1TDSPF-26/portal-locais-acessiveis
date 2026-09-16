86f1f258ac6c6b170a61577afd259f5617336a9c
Kauã Silva de Jesus <kauansilva1465285@gmail.com>
2026-09-16 15:51:08 -0300

    feat: implementa estrutura inicial do menu mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,27 @@
 import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
 import "./Header.css";
 
+
 export function Header() {
+  const [menuAberto, setMenuAberto] = useState(false);
+
   return (
     <header>
+
+    <button
+      className="menuMobileButton"
+      type="button"
+      aria-expanded={menuAberto}
+      aria-controls="menu-principal"
+      onClick={() => setMenuAberto(!menuAberto)}>
+  Menu
+  </button>
+
       <Link to="/" className="portal-title">
         <span>Portal de Locais e Serviços Acessíveis</span>
       </Link>
-      <nav className="mainNav" aria-label="Menu Principal">
+      <nav id="menu-principal"   className={`mainNav ${menuAberto ? "aberto" : ""}`} aria-label="Menu Principal">
         <ul>
 
           <li>
