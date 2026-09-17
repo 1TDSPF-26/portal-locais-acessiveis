b5ddc749263ae71bb41c3d3fd6263ddfca9f921c
caiomarqx <caio.marques111107@gmail.com>
2026-09-16 21:49:33 -0300

    feat: integra links do footer às rotas

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,29 @@
+import { Link } from "react-router-dom";
+
 export function Footer() {
   return (
     <footer>
       <p>Portal de Locais e Serviços Acessíveis</p>
+
+      <nav aria-label="Navegação do rodapé">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+
+          <li>
+            <Link to="/locais">Locais</Link>
+          </li>
+
+          <li>
+            <Link to="/cadastrar">Cadastro</Link>
+          </li>
+
+          <li>
+            <Link to="/sobre">Sobre</Link>
+          </li>
+        </ul>
+      </nav>
     </footer>
   );
 }
