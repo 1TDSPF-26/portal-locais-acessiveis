3ebc9ecf7490eb83939a3085a04d9e4f8e3abc88
giovvane <giovanedc007@gmail.com>
2026-09-26 11:43:43 -0300

    feat: Transformando main em section em DetalhesLocal.tsx e verificando links

--- src/pages/DetalhesLocal/DetalhesLocal.tsx
+++ src/pages/DetalhesLocal/DetalhesLocal.tsx
@@ -4,10 +4,10 @@ function DetalhesLocal() {
   const { id } = useParams();
 
   return (
-    <main>
+    <section>
       <h1>Detalhes do local</h1>
       <p>ID recebido pela URL: {id}</p>
-    </main>
+    </section>
   );
 }
 
